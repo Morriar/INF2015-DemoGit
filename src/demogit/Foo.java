c6737d3aef2b6532d9123d3371e3efd79d53a10d
Alexandre Terrasa <alexandre@moz-code.org>
2015-10-07 12:00:26 -0300

    Ajoute la boucle infinie dans Foo

--- src/demogit/Foo.java
+++ src/demogit/Foo.java
@@ -4,5 +4,8 @@ package demogit;
 public class Foo {
     public void foo() {
         System.out.println("Hello Foo!");
+        while(true) {
+            System.out.println("infinite loop...");
+        }
     }
 }
