cc82e5012c3b18649ba732aa68a9feb4942953ea
Alexandre Terrasa <alexandre@moz-code.org>
2015-10-07 12:14:55 -0300

    Ajoute une nouvelle ligne dans DemoGit

--- src/demogit/DemoGit.java
+++ src/demogit/DemoGit.java
@@ -11,6 +11,7 @@ public class DemoGit {
      */
     public static void main(String[] args) {
         System.out.println("Bonjour le monde !");
+        System.out.println("Bonjour le monde !");
     }
     
 }
